a52efd0d34abd8eb9a3fcdeb0463efa003ff6149
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-05-29 23:46:07 -0300

    Update 2_Trending_Keywords.py

--- pages/2_Trending_Keywords.py
+++ pages/2_Trending_Keywords.py
@@ -1,5 +1,16 @@
+import streamlit as st
+import pandas as pd
 from collections import Counter
 import re
+
+df = pd.read_csv("fashion_news.csv")
+
+all_text = (
+    df["titulo"].fillna("") + " " +
+    df["resumo"].fillna("")
+).str.lower()
+
+text = " ".join(all_text)
 
 words = re.findall(r'\b[a-z]+\b', text)
 
