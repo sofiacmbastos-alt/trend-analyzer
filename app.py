ae5de899a6aff6bb9295bf2e45618b33d5163ac9
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-06-11 11:19:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -113,10 +113,7 @@ st.markdown("""
 # LOAD DATA
 # ----------------------
 
-df1 = pd.read_csv("fashion_news2.csv")
-df2 = pd.read_csv("fashion_news.csv")
-
-df = pd.concat([df1, df2], ignore_index=True)
+df = pd.read_csv("merged.csv")
 
 # ----------------------
 # TREND EXTRACTION
