bfe0b3a9db2bef24526913524bdc2d2a6dad8f4c
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-06-18 18:32:55 -0300

    Update ModeRn.py

--- ModeRn.py
+++ ModeRn.py
@@ -198,7 +198,7 @@ components.html("""
 
 <div class="slider-container">
     <img class="slide" src="https://i.pinimg.com/736x/cd/fa/ca/cdfaca6cdbf0b9ca08b6d78c7ada478e.jpg">
-    <img class="slide" src="https://i.pinimg.com/1200x/d9/c5/25/d9c525f4ad3eafd8d85cb671fcd26446.jpg">
+    <img class="slide" src="https://i.pinimg.com/736x/d4/51/94/d45194d1756faa5d3c79dae0e535866c.jpg">
     <img class="slide" src="https://i.pinimg.com/736x/69/20/af/6920afc2deced780759f7737c01e639b.jpg">
 </div>
 """, height=420)
