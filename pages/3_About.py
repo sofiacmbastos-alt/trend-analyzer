e0228bd94fd27cb29dec3825eadfd63831dca813
sofiacmbastos-alt <sofiacmbastos@gmail.com>
2026-06-19 00:57:18 -0300

    Adjust image width in 3_About.py
    Increased image width from 320 to 340 pixels.

--- pages/3_About.py
+++ pages/3_About.py
@@ -114,7 +114,7 @@ with col1:
     left, center, right = st.columns([1,2,1])
 
     with center:
-        st.image("fotositesofia.jpeg", width=320)
+        st.image("fotositesofia.jpeg", width=340)
 
     st.markdown("""
     <div style="
